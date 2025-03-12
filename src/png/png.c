d67333c6ef4f7fef877f61c7e360c7c678aaf6d0
giji676 <tvtengoxachidze@gmail.com>
2025-03-12 19:57:52 +0000

    Added chunk layout read, CRC not yet validated

--- src/png/png.c
+++ src/png/png.c
@@ -3,6 +3,16 @@
 #include <stdlib.h>
 #include "./png.h"
 
+void png_printChunkLayout(struct png_chunkLayout *chunkLayout) {
+    printf("length: %u\n", chunkLayout->length);
+    printf("chunkType: %.4s\n", chunkLayout->chunkType);
+    printf("chunkData: ");
+    for (int i = 0; i < (int)sizeof(chunkLayout->chunkData); ++i) {
+        printf("%d ", chunkLayout->chunkData[i]);
+    }
+    printf("\n");
+    printf("crc: %u\n", chunkLayout->crc);
+}
 
 void png_printFileSignature(struct png_fileSignature *fileSignature) {
     printf("signature: ");
@@ -10,6 +20,36 @@ void png_printFileSignature(struct png_fileSignature *fileSignature) {
         printf("%02x ", (unsigned char)fileSignature->signature[i]);
     }
     printf("\n");
+}
+
+int png_readChunkLayout(FILE *fptr, struct png_chunkLayout *chunkLayout) {
+    if (fread(chunkLayout,
+              (sizeof(chunkLayout->length) + sizeof(chunkLayout->chunkType)),
+              1, fptr) != 1) {
+        printf("Failed to read chunk layout\n");
+        return -1;
+    }
+
+    chunkLayout->length = __builtin_bswap32(chunkLayout->length);
+    chunkLayout->chunkData = (uint8_t *)malloc(chunkLayout->length);
+    if (chunkLayout->chunkData == NULL) {
+        printf("Failed to allocate memory for chunk data\n");
+        return -1;
+    }
+
+    if (fread(chunkLayout->chunkData, chunkLayout->length, 1, fptr) != 1) {
+        printf("Failed to read chunk data\n");
+        free(chunkLayout->chunkData);
+        return -1;
+    }
+
+    if (fread(chunkLayout, sizeof(chunkLayout->crc), 1, fptr) != 1) {
+        printf("Failed to read chunk crc\n");
+        free(chunkLayout->chunkData);
+        return -1;
+    }
+    chunkLayout->crc = __builtin_bswap32(chunkLayout->crc);
+    return 1;
 }
 
 int png_readFileSignature(FILE *fptr, struct png_fileSignature *fileSignature) {
@@ -33,6 +73,15 @@ void png_open(char filename[]) {
         return;
     }
     printf("\n");
-    printf("File Header\n");
+    printf("File Signature\n");
     png_printFileSignature(&png_fileSignature);
+
+    struct png_chunkLayout png_chunkLayout;
+    if (png_readChunkLayout(fptr, &png_chunkLayout) != 1) {
+        return;
+    }
+    printf("\n");
+    printf("Chunk Layout\n");
+    png_printChunkLayout(&png_chunkLayout);
+    fclose(fptr);
 }
--- src/png/png.h
+++ src/png/png.h
@@ -9,8 +9,17 @@ struct __attribute__((packed)) png_fileSignature {
     char signature[8];
 };
 
+struct __attribute__((packed)) png_chunkLayout {
+    uint32_t length;
+    char chunkType[4];
+    uint8_t *chunkData;
+    uint32_t crc;
+};
+
 void png_printFileSignature(struct png_fileSignature *fileSignature);
+void png_printChunkLayout(struct png_chunkLayout *chunkLayout);
 int png_readFileSignature(FILE *fptr, struct png_fileSignature *fileSignature);
+int png_readChunkLayout(FILE *fptr, struct png_chunkLayout *chunkLayout);
 void png_open(char filename[]);
 
 #endif  // PNG_H
