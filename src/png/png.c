08bc36ca37893ea1f737f7965a49c27ceb5a29fb
giji676 <tvtengoxachidze@gmail.com>
2025-03-13 00:01:46 +0000

    Added IHDR to chunk layout reader

--- src/png/png.c
+++ src/png/png.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include "./png.h"
 
-void png_printChunkLayout(struct png_chunkLayout *chunkLayout) {
-    printf("length: %u\n", chunkLayout->length);
-    printf("chunkType: %.4s\n", chunkLayout->chunkType);
-    printf("chunkData: ");
-    for (int i = 0; i < (int)sizeof(chunkLayout->chunkData); ++i) {
-        printf("%d ", chunkLayout->chunkData[i]);
+void png_printIHDR(struct png_IHDR *ihdr) {
+    printf("\n");
+    printf("IHDR\n");
+    printf("width: %u\n", ihdr->width);
+    printf("height: %u\n", ihdr->height);
+    printf("bitDepth: %u\n", ihdr->bitDepth);
+    printf("colorType: %u\n", ihdr->colorType);
+    printf("compressionMethod: %u\n", ihdr->compressionMethod);
+    printf("filterMethod: %u\n", ihdr->filterMethod);
+    printf("interlaceMethod: %u\n", ihdr->interlaceMethod);
+}
+
+void png_printChunkLayout(struct png_chunkLayout *chunk) {
+    printf("\n");
+    printf("Chunk Layout\n");
+    printf("length: %u\n", chunk->length);
+    printf("chunkType: %.4s\n", chunk->chunkType);
+    if (strncmp(chunk->chunkType, "IHDR", 4) == 0 && chunk->length == 13) {
+        png_printIHDR((struct png_IHDR *)chunk->chunkData);
+    } else {
+        printf("chunkData: ");
+        for (uint32_t i = 0; i < chunk->length; ++i) {
+            printf("%d ", ((uint8_t *)chunk->chunkData)[i]);
+        }
     }
     printf("\n");
-    printf("crc: %u\n", chunkLayout->crc);
+    printf("crc (Hex): %08X\n", chunk->crc);
 }
 
 void png_printFileSignature(struct png_fileSignature *fileSignature) {
+    printf("\n");
+    printf("File Signature\n");
     printf("signature: ");
     for (int i = 0; i < (int)sizeof(fileSignature->signature); ++i) {
         printf("%02x ", (unsigned char)fileSignature->signature[i]);
@@ -22,33 +43,41 @@ void png_printFileSignature(struct png_fileSignature *fileSignature) {
     printf("\n");
 }
 
-int png_readChunkLayout(FILE *fptr, struct png_chunkLayout *chunkLayout) {
-    if (fread(chunkLayout,
-              (sizeof(chunkLayout->length) + sizeof(chunkLayout->chunkType)),
+int png_readChunkLayout(FILE *fptr, struct png_chunkLayout *chunk) {
+    if (fread(chunk,
+              (sizeof(chunk->length) + sizeof(chunk->chunkType)),
               1, fptr) != 1) {
         printf("Failed to read chunk layout\n");
         return -1;
     }
 
-    chunkLayout->length = __builtin_bswap32(chunkLayout->length);
-    chunkLayout->chunkData = (uint8_t *)malloc(chunkLayout->length);
-    if (chunkLayout->chunkData == NULL) {
+    chunk->length = __builtin_bswap32(chunk->length);
+    chunk->chunkData = (void *)malloc(chunk->length);
+    if (chunk->chunkData == NULL) {
         printf("Failed to allocate memory for chunk data\n");
         return -1;
     }
 
-    if (fread(chunkLayout->chunkData, chunkLayout->length, 1, fptr) != 1) {
+    if (fread(chunk->chunkData, chunk->length, 1, fptr) != 1) {
         printf("Failed to read chunk data\n");
-        free(chunkLayout->chunkData);
+        free(chunk->chunkData);
         return -1;
     }
 
-    if (fread(chunkLayout, sizeof(chunkLayout->crc), 1, fptr) != 1) {
+    if (strncmp(chunk->chunkType, "IHDR", 4) == 0 && chunk->length == 13) {
+        ((struct png_IHDR *)chunk->chunkData)->width =
+            __builtin_bswap32(((struct png_IHDR *)chunk->chunkData)->width);
+        ((struct png_IHDR *)chunk->chunkData)->height =
+            __builtin_bswap32(((struct png_IHDR *)chunk->chunkData)->height);
+    }
+
+    if (fread(&chunk->crc, sizeof(chunk->crc), 1, fptr) != 1) {
         printf("Failed to read chunk crc\n");
-        free(chunkLayout->chunkData);
+        free(chunk->chunkData);
         return -1;
     }
-    chunkLayout->crc = __builtin_bswap32(chunkLayout->crc);
+    chunk->crc = __builtin_bswap32(chunk->crc);
+
     return 1;
 }
 
@@ -72,16 +101,13 @@ void png_open(char filename[]) {
     if (png_readFileSignature(fptr, &png_fileSignature) != 1) {
         return;
     }
-    printf("\n");
-    printf("File Signature\n");
     png_printFileSignature(&png_fileSignature);
 
     struct png_chunkLayout png_chunkLayout;
     if (png_readChunkLayout(fptr, &png_chunkLayout) != 1) {
         return;
     }
-    printf("\n");
-    printf("Chunk Layout\n");
     png_printChunkLayout(&png_chunkLayout);
     fclose(fptr);
+    free(png_chunkLayout.chunkData);
 }
--- src/png/png.h
+++ src/png/png.h
@@ -12,8 +12,18 @@ struct __attribute__((packed)) png_fileSignature {
 struct __attribute__((packed)) png_chunkLayout {
     uint32_t length;
     char chunkType[4];
-    uint8_t *chunkData;
+    void *chunkData;
     uint32_t crc;
+};
+
+struct __attribute__((packed)) png_IHDR {
+    uint32_t width;
+    uint32_t height;
+    uint8_t bitDepth;
+    uint8_t colorType;
+    uint8_t compressionMethod;
+    uint8_t filterMethod;
+    uint8_t interlaceMethod;
 };
 
 void png_printFileSignature(struct png_fileSignature *fileSignature);
